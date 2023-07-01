1fd88db3d758fe5b57b81d82f6a733aa2f2064fd
Pavlyuk <a@b.c>
2023-07-01 19:45:47 +0400

    dz2

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int numb;
+
+Console.WriteLine("Введите трёхначное число");
+numb= int.Parse(Console.ReadLine());
+
+string numbstr = numb.ToString();
+
+if (numbstr.Length != 3){
+    Console.WriteLine($"это не трёхзначное число");
+}
+else{
+    numb = numb /10;
+    Console.WriteLine($"Вторая цифра - {numb % 10}");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 2679 -> 6
+
+void trinumb(int numb, int i ){
+    while (i > 3){
+    numb = numb /10;
+    i = i-1;
+}
+Console.WriteLine(numb % 10);
+}
+
+//-------------------------------------
+
+int numb, i;
+
+Console.WriteLine("Введите число");
+numb= int.Parse(Console.ReadLine());
+
+string numbstr = numb.ToString();
+i = numbstr.Length;
+if (i < 3){
+    Console.WriteLine("третьей цифры нет");
+}
+else{
+trinumb(numb, i);
+}
+ 
